11f88c255897eca3051f03acf165353e4be504f3
Rylan Bauermeister <apple@MacBook-Pro-6.local>
2019-05-31 17:49:56 -0700

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import {removeQuote, upvoteQuote, downvoteQuote} from '../actions/quotes'
 
 class Quotes extends Component {
+
+  onClickUpvote(id){
+    this.props.upvoteQuote(id);
+  }
+  onClickDownvote(id){
+    this.props.downvoteQuote(id);
+  }
+  onClickRemove(id){
+    this.props.removeQuote(id);
+  }
 
   render() {
     return (
@@ -15,11 +26,19 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
+              {this.props.quotes.map(quote => {
 
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              return <div>
+                <p>"{quote.content}"</p>
+                <p>-{quote.author}</p>
+                <div className="btn-group">
+                  <button onClick={() => this.onClickUpvote(quote.quoteId)} type="button" className="btn btn-primary">Upvote</button>
+                  <button onClick={() => this.onClickDownvote(quote.quoteId)} type="button" className="btn btn-primary">Downvote</button>
+                  <button onClick={() => this.onClickRemove(quote.quoteId)} type="button" className="btn btn-primary">Delete</button>
+                </div>
+                <p>Votes: {quote.votes}</p>
+              </div>})
+            }
             </div>
           </div>
         </div>
@@ -29,4 +48,12 @@ class Quotes extends Component {
 }
 
 //add arguments to connect as needed
-export default connect()(Quotes);
+const mapDispatchToProps = dispatch => ({
+  removeQuote: id => dispatch(removeQuote(id)),
+  upvoteQuote: id => dispatch(upvoteQuote(id)),
+  downvoteQuote: id => dispatch(downvoteQuote(id))
+})
+
+const mapStateToProps = state => ({quotes: state.quotes})
+
+export default connect(mapStateToProps, {removeQuote, upvoteQuote, downvoteQuote})(Quotes);
